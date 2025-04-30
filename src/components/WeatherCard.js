9ff12b85660cb15e2829bbfc12ee34fd35c52d0b
miguel-agm-dev <miguel.agm.dev@gmail.com>
2025-04-30 13:31:51 +0200

    Informacion del clima de localidad

---
+++ src/components/WeatherCard.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const WeatherCard = ({ data }) => {
+  return (
+    <div>
+      <h2>{data.name}</h2>
+      <p>Temperatura: {data.main.temp}°C</p>
+      <p>Condición: {data.weather[0].description}</p>
+    </div>
+  );
+};
+
+export default WeatherCard;
