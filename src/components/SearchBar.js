9c8d2feb2f1530cff21046ef3efc23d9eb366e54
miguel-agm-dev <miguel.agm.dev@gmail.com>
2025-04-30 13:33:14 +0200

    Buscar localidad

---
+++ src/components/SearchBar.js
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+import { getWeather } from './weatherApi';
+
+const SearchBar = ({ addFavorite, setWeatherData }) => {
+  const [city, setCity] = useState('');
+
+  const handleSearch = async () => {
+    const weather = await getWeather(city);
+    if (weather) {
+      setWeatherData(weather);
+    }
+  };
+
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="Buscar localidad"
+        value={city}
+        onChange={(e) => setCity(e.target.value)}
+      />
+      <button onClick={handleSearch}>Buscar</button>
+      <button onClick={() => addFavorite(city)}>Guardar localidad</button>
+    </div>
+  );
+};
+
+export default SearchBar;
