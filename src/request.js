aeddc48039f85b2af62367b3cf8bd38b10f460ae
miguel-agm-dev <miguel.agm.dev@gmail.com>
2025-04-30 16:37:18 +0200

    Funciona buscador

--- src/request.js
+++ src/request.js
@@ -3,13 +3,14 @@ import axios from 'axios';
 // Function to get the weather
 export const getWeather = async (city) => {
   const apiKey = 'ac3e959add0b4bd5b11112137253004';
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city},es&appid=${apiKey}&units=metric&lang=es`;
+  const url = `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}&aqi=no`;
 
   try {
     const response = await axios.get(url);
     return response.data;
   } catch (error) {
     console.error('Error al obtener el tiempo', error);
+    alert('Error al obtener el clima. Verifica el nombre de la ciudad.');
     return null;
   }
 };
