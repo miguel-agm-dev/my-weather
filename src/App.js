69940f7030f735a3d8998ee4b0b5031ea5bb1a17
miguel-agm-dev <miguel.agm.dev@gmail.com>
2025-04-30 13:38:39 +0200

    App con funciones main

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react';
+import WeatherCard from './components/WeatherCard';
+import SearchBar from './components/SearchBar';
 
-function App() {
+const App = () => {
+  const [weatherData, setWeatherData] = useState([]);
+  const [favorites, setFavorites] = useState(() => {
+    
+    // Load saved locations from localStorage when loading the app
+    const savedFavorites = localStorage.getItem('favorites');
+    return savedFavorites ? JSON.parse(savedFavorites) : [];
+  });
+
+  useEffect(() => {
+    // Save favorite locations to localStorage
+    localStorage.setItem('favorites', JSON.stringify(favorites));
+  }, [favorites]);
+
+  const addFavorite = (city) => {
+    if (favorites.length < 3) {
+      setFavorites([...favorites, city]);
+    } else {
+      alert('Solo se permite guardar 3 localidades.');
+    }
+  };
+
+  const removeFavorite = (city) => {
+    setFavorites(favorites.filter((favorite) => favorite !== city));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>My Weather</h1>
+      <SearchBar addFavorite={addFavorite} setWeatherData={setWeatherData} />
+      <div>
+        {weatherData && <WeatherCard data={weatherData} />}
+      </div>
+      <div>
+        <h2>Localidades Favoritas</h2>
+        {favorites.map((city) => (
+          <div key={city}>
+            <span>{city}</span>
+            <button onClick={() => removeFavorite(city)}>Eliminar</button>
+          </div>
+        ))}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { getWeather } from './weatherApi';
 
 const SearchBar = ({ addFavorite, setWeatherData }) => {
